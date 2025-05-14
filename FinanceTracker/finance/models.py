ddf9f0b696f4f61f20e23bd2ca261254efd672d7
Ankit Baingane <ankitbaingane157@gmail.com>
2025-05-15 02:58:12 +0530

    new file added

--- FinanceTracker/finance/models.py
+++ FinanceTracker/finance/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    TYPE_CHOICE = [('Income', 'Income'), ('Expense', 'Expense')]
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=10, choices=TYPE_CHOICE, default='Expense')
+
+    def __str__(self):
+        return self.name
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    description = models.TextField(blank=True)
+    type = models.CharField(max_length=10, choices=[('Income', 'Income'), ('Expense', 'Expense')], default='Expense')
+    
+    def __str__(self):
+        return f"{self.type} : {self.amount}"
+    
+    
--- FinanceTracker/finance/views.py
+++ FinanceTracker/finance/views.py
@@ -1,11 +1,42 @@
 from django.shortcuts import render
 from .forms import *
+from .models import *
+from django.db.models import Sum
+from datetime import date
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
-
+@login_required
 def dashboard(request):
-    return render(request, 'dashboard.html')
-    # return HttpResponse("Hello, world. You're at the finance index.")
+    user = request.user
+    
+    income = Transaction.objects.filter(user=user, type='Income').aggregate(Sum('amount'))['amount__sum'] or 0
+    expenses = Transaction.objects.filter(user=user, type='Expense').aggregate(Sum('amount'))['amount__sum'] or 0
+    balance = income - expenses
+    
+    recent_transactions = Transaction.objects.filter(user=user).order_by('-date')[:5]
+    
+    category_data = (
+        Transaction.objects.filter(user=user, type='Expense')
+        .values('category__name')
+        .annotate(total=Sum('amount'))
+    )
+    
+    labels = [entry['category__name'] for entry in category_data]
+    data = [entry['total'] for entry in category_data]
+    
+    context = {
+        balance: balance,
+        'income': income,
+        'expenses': expenses,
+        'recent_transactions': recent_transactions,
+        'labels': labels,
+        'data': data,
+    }
+    
+    return render(request, 'dashboard.html', context)
+  
 
 def register(request):
     if request.method == 'POST':
@@ -30,3 +61,5 @@ def login(request):
 def logout(request):
     return render(request, 'login.html')
 
+def transaction(request):
+    return render(request, 'transaction.html')
